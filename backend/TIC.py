9a5904ae94c5844f8699ceece53b04fa4301d083
Daniel Marinho <danielmarinho8@hotmail.com>
2024-10-07 14:37:43 -0300

    alterações SOM

--- backend/TIC.py
+++ backend/TIC.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, send_file
 from flask_cors import CORS  # Importa CORS
 import numpy as np
 from sklearn.datasets import load_iris
@@ -8,18 +8,18 @@ import os
 import tempfile
 
 app = Flask(__name__)
-CORS(app)  # Adiciona CORS ao aplicativo Flask
+CORS(app)
 
-# Função para calcular a distância euclidiana
+#Calcula distância euclidiana
 def euclidean_distance(x, y):
     return np.sqrt(np.sum((x - y) ** 2))
 
-# Função para encontrar o neurônio vencedor
+#Encontra o neurônio vencedor
 def find_bmu(som, sample):
     bmu_idx = np.argmin([euclidean_distance(neuron, sample) for neuron in som.reshape(-1, som.shape[2])])
     return np.unravel_index(bmu_idx, som.shape[:2])
 
-# Função para atualizar os pesos
+#Atualiza os pesos dos neurônios
 def update_weights(som, sample, bmu_idx, iteration, max_iterations, learning_rate, radius):
     learning_rate_decay = learning_rate * np.exp(-iteration / max_iterations)
     radius_decay = radius * np.exp(-iteration / max_iterations)
@@ -31,7 +31,7 @@ def update_weights(som, sample, bmu_idx, iteration, max_iterations, learning_rat
                 influence = np.exp(-distance_to_bmu**2 / (2 * (radius_decay ** 2)))
                 som[i, j, :] += influence * learning_rate_decay * (sample - som[i, j, :])
 
-# Função para treinar o SOM
+#Treinar SOM
 def train_som(X, som, max_iterations=1000, learning_rate=0.5, radius=3):
     for iteration in range(max_iterations):
         for sample in X:
@@ -40,21 +40,21 @@ def train_som(X, som, max_iterations=1000, learning_rate=0.5, radius=3):
 
 @app.route('/train-som', methods=['POST'])
 def train_som_endpoint():
-    # Carregar o dataset Iris e normalizar
+    #Carregar dataset e normalização dos dados
     iris = load_iris()
     X = iris.data
     y = iris.target
     scaler = MinMaxScaler()
     X_scaled = scaler.fit_transform(X)
 
-    # Inicializar a rede SOM
+    #Inicializar SOM
     som_grid_size = 7
     som = np.random.rand(som_grid_size, som_grid_size, X.shape[1])
 
-    # Treinar o SOM
+    #Treinar SOM
     train_som(X_scaled, som, max_iterations=500, learning_rate=0.5, radius=3)
 
-    # Gerar visualização
+    #Plot
     plt.figure(figsize=(10, 10))
     colors = ['r', 'g', 'b']
     markers = ['o', 's', 'D']
@@ -67,9 +67,8 @@ def train_som_endpoint():
     plt.xticks(np.arange(som_grid_size + 1))
     plt.yticks(np.arange(som_grid_size + 1))
     plt.grid()
-    plt.title("Self-Organizing Map (SOM) - Implementação com Flask")
 
-    # Salvar o gráfico em um diretório temporário
+    #Salvar gráfico
     try:
         temp_dir = tempfile.gettempdir()
         image_path = os.path.join(temp_dir, 'som_plot.png')  # Salvar no diretório temporário
